229319d79e2fb0a51ea666c9d1aa17389b558b70
kmeeth <matijakup@gmail.com>
2021-07-25 22:03:07 +0200

    Created dummy functions that currently do nothing in small_math.cpp

--- src/small_math.cpp
+++ src/small_math.cpp
@@ -1,1 +1,16 @@
-
+#include "small_math.h"
+namespace small_math
+{
+	int add(int a, int b)
+	{
+		return 0; //dummy
+	}
+	int sub(int a, int b)
+	{
+		return 0; //dummy
+	}
+	int mul(int a, int b)
+	{
+		return 0; //dummy
+	}
+}
